485da41cc1da01031aa2441bbfcb00bbed8d27ed
Galdino <pedro.wollmann@gmail.com>
2025-04-28 18:35:18 -0300

    Adiciona setters e getters na classe Disciplina

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -2,6 +2,12 @@ public class Disciplina {
     private String codigo;
     private Integer capacidadeMaxima;
 
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
 
+    public String getCodigo(){
+        return this.codigo;
+    }
 }
 
